d957b63a205fe47fa10432388d8ef94cde734219
AhmedElminshawy89 <https://github.com/AhmedElminshawy89/>
2024-12-01 21:42:26 +0200

    modifications

--- src/host/Host.js
+++ src/host/Host.js
@@ -1,7 +1,7 @@
-// const host = "http://localhost:8000";
-
-// export default host;
-
-const host = "https://www.gharamsoltan.com/BackEnd";
+const host = "http://localhost:8000";
 
 export default host;
+
+// const host = "https://www.gharamsoltan.com/BackEnd";
+
+// export default host;
